2b201be9dc95ca5bf9b5e7bd4f347feb4a2b17d1
Rana Emad <rana3emad@gmail.com>
2020-06-11 15:51:21 +0200

    Updates

---
+++ precache-manifest.24335bc04779b70bce161608fbff3f64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9d9114a7f3909b88704e0e4fa8444b4",
+    "url": "/text-recorder/index.html"
+  },
+  {
+    "revision": "53c796c02edf1432eb7d",
+    "url": "/text-recorder/static/css/main.e330dce1.chunk.css"
+  },
+  {
+    "revision": "c95bd63d4c54abf2ece6",
+    "url": "/text-recorder/static/js/2.e5485dc4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/text-recorder/static/js/2.e5485dc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53c796c02edf1432eb7d",
+    "url": "/text-recorder/static/js/main.bca13e06.chunk.js"
+  },
+  {
+    "revision": "c72ca23b82032025caa8",
+    "url": "/text-recorder/static/js/runtime-main.73e8b258.js"
+  }
+]);
