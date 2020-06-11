8d4a7053c5856eeb90311b2fa510ae749638e27c
Rana Emad <rana3emad@gmail.com>
2020-06-11 10:22:36 +0200

    project setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import TextRecorder from "./components/TextRecorder/TextRecorder";
+
+function App() {
+  return (
+    <div className="app">
+      <TextRecorder />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TextRecorder/TextRecorder.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./TextRecorder.css";
+
+class TextRecorder extends React.Component {
+  render() {
+    return <div className="text-recaorder"></div>;
+  }
+}
+
+export default TextRecorder;
