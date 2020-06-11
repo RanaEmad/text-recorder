9c5086217c1a98e10f9b37593fbb9443f12045a5
Rana Emad <rana3emad@gmail.com>
2020-06-11 10:49:57 +0200

    basic component structure

--- src/components/TextRecorder/TextRecorder.js
+++ src/components/TextRecorder/TextRecorder.js
@@ -3,7 +3,22 @@ import "./TextRecorder.css";
 
 class TextRecorder extends React.Component {
   render() {
-    return <div className="text-recaorder"></div>;
+    return (
+      <div className="text-recorder">
+        <h1 className="header">Text Recorder</h1>
+        <textarea
+          className="text"
+          rows="10"
+          placeholder="Press Record and start typing..."
+        ></textarea>
+        <div className="controls">
+          <button className="btn">Record</button>
+          <button className="btn">Stop Recording</button>
+          <button className="btn">Play</button>
+          <button className="btn">Reset</button>
+        </div>
+      </div>
+    );
   }
 }
 
